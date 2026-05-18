6ad2da999d05c86e36ebce0f6185b89a5a4a841c
PhuNguyen8903 <ngngocphu8903@gmail.com>
2026-05-18 17:45:35 +0700

    Login HTML+MockDatta

--- js/mock-data.js
+++ js/mock-data.js
@@ -1,1 +1,10 @@
-const Users = []
+const Users = [
+    { 
+        Username: "Admin",
+        password: "Admin@123",
+        profileImg: "",
+        fullName: "Phune",
+        gender: "Male",
+        phoneNumber: "123456789123",
+    },
+]
